b9740e4aeebc35e69941e3f9ff29945f952d1100
Ujjwal.92425 <ujjwal7739@gmail.com>
2023-08-19 13:47:42 +0530

    19aug

--- c++18aug.cpp
+++ c++18aug.cpp
@@ -53,10 +53,28 @@ int main() {
         cout<<e[i].name<<endl;
     }
     cout<<endl;
-    return 0;
-    
-    
+    return 0; 
+}
 
+//enumerations
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    enum days{sun,mon,tues,wed,thur,fri,sat};
+    days day1,day2;
+    day1=sun;
+    day2=fri;
+    cout<<day1<<"\t"<<day2;
+    if(day1>day2)
+    {
+        cout<<"day 1 comes after day2";
+    }
+    else
+    {
+        cout<<"day 1 comes before day 2";
+    }
 }
 
 

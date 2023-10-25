7a2dc79b1ad2a3ec32a5d74875c388a4be942436
Ujjwal.92425 <ujjwal7739@gmail.com>
2023-10-25 10:11:13 +0530

    25 oct

---
+++ AlaiBlai.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int p;
+    cin>>p;
+    cout<<p<<" ";
+
+cout<<n<<endl;
+for(i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+}
--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,33 +1,34 @@
-// #include <iostream>
-// using namespace std;
-// class one {
+#include <iostream>
+using namespace std;
+class one {
     
-//     public:
-//     int x,y;
-//     void in() {
-//         cout<<"Enter the data "<<endl;
-//         cin>>x>>y;
-//     }
-// };
-// class two : public one {
+    public:
+    int x,y;
+    void in() {
+        cout<<"Enter the data "<<endl;
+        cin>>x>>y;
+    }
+};
+class two : public one {
     
-//     public:
-//     int z;
-//     void take() {
-//         cout<<"Enter Z : "<<endl;
-//         cin>>z;
-//     }
-//     void dis() {
-//         cout<<x+y+z;
-//         }
-// };
-// int main() {
-//     two t1;
-//     t1.in();
-//     t1.take();
-//     t1.dis();
+    public:
+    int z;
+    void take() {
+        cout<<"Enter Z : "<<endl;
+        cin>>z;
+    }
+    void dis() {
+        cout<<x+y+z;
+        }
+};
+int main() {
+    two t1;
+    t1.in();
+    t1.take();
+    t1.dis();
        
-// }
+}
+
 
 /*#include<bits/stdc++.h>
 using namespace std;
